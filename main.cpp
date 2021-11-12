681b08e04a5290c215cbf669649ca328d46bfee0
Lucas PELLIER <lpellier@e2r1p7.clusters.42paris.fr>
2021-11-12 17:21:17 +0100

    lovin' the bug life

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "test.hpp"
+#include <stdlib.h>
 
 int main() {
 	std::vector<int>	template_vector(0);
@@ -7,9 +8,9 @@ int main() {
 	// const std::vector<int>		actual_const_vector(template_vector);	
 	// const ft::vector<int>		my_const_vector(template_vector.begin(), template_vector.end());
 	// std::vector<int>			actual_vector(template_vector);
-	std::vector<int>				my_vector(template_vector.begin(), template_vector.end());
+	ft::vector<int>				my_vector(template_vector.begin(), template_vector.end());
 
-	std::vector<int>::const_iterator it_foo = my_vector.begin();
+	ft::vector<int>::const_iterator it_foo = my_vector.begin();
 	std::cout << *it_foo;
 
 	// testIterator(actual_vector, my_vector);
