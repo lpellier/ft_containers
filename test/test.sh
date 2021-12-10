1cf6d5dca3d4e6bfd5847e7501dd2bb1280ffcf0
Lucas PELLIER <pellierlucas@gmail.com>
2021-12-10 12:38:21 +0100

    leak check

--- test/test.sh
+++ test/test.sh
@@ -10,35 +10,44 @@ RESET="\e[0m"
 
 STD_VECTOR="const std::vector<int>"
 FT_VECTOR="const ft::vector<int>"
-
-function _compile () {
-	clang++ -v -Wall -Werror -Wextra -std=c++98 -D CONTAINER="$1" $2 2> $2".compile_error" 
-	./a.out > "$2$3"
-	rm -rf ./a.out
-}
+GRADE="$GREEN$OKAY"
 
 rm -rf */*.actual_output
 rm -rf */*.your_output
 rm -rf */*.compile_error
+rm -rf */*.leaks_error
 for folder in */; do
 	for filename in $folder*.cpp; do
-		GRADE="$GREEN$OKAY"
-		_compile "$STD_VECTOR" "$filename" ".actual_output"
-		_compile "$FT_VECTOR" "$filename" ".your_output"
+		clang++ -Wall -Werror -Wextra -std=c++98 -D CONTAINER="$STD_VECTOR" $filename 2> $filename".compile_error"
+		./a.out > "$filename.actual_output"
+		rm -rf a.out
+		
+		clang++ -Wall -Werror -Wextra -std=c++98 -D CONTAINER="$FT_VECTOR" $filename 2> $filename".compile_error"
 		if [ $? -ne 0 ]; then
 			GRADE="$RED$NONO Compile error"
 			echo -e "$RESET\e[36m$filename : $GRADE$RESET"
 			continue
 		fi
+		
+		valgrind --error-exitcode=2 --leak-check=full --show-leak-kinds=all ./a.out 1> $filename".your_output" 2> $filename".leaks_error"
+		if [ $? -ne 0 ]; then
+			GRADE="$RED$NONO Leaks detected"
+			echo -e "$RESET\e[36m$filename : $GRADE$RESET"
+			continue
+		fi
+		rm -rf a.out
+		
 		diff $filename".your_output" $filename".actual_output" 1> /dev/null 2> /dev/null
 		if [ $? -ne 0 ]; then
 			GRADE="$RED$NONO Different output"
 			echo -e "$RESET\e[36m$filename : $GRADE$RESET"
 			continue
 		fi
+		
 		rm -rf $filename".your_output"
 		rm -rf $filename".actual_output"
 		rm -rf $filename".compile_error"
 		echo -e "$RESET\e[36m$filename : $GRADE$RESET"
+		GRADE="$GREEN$OKAY"
 	done
 done
